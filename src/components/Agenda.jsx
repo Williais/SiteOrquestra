beeccba545a25e488109da3f3d6cea63d51de069
Willian Padilha <45665895+Williais@users.noreply.github.com>
2025-12-08 18:52:55 -0300

    Component Agenda

--- src/components/Agenda.jsx
+++ src/components/Agenda.jsx
@@ -3,7 +3,23 @@ import '../style/agenda.css'
 function Agenda() {
     return(
         <div>
-            <h1>Agenda</h1>
+            <div className="header-agenda">
+                <h1>Agenda de Concertos</h1>
+            </div>
+
+            <div className="container-agenda">
+                <div className='container-data'>
+                    <h1>13</h1>
+                    <h3>DEZ</h3>
+                    <p>19:30</p>
+                </div>
+
+                <div className="container-infos">
+                    <h1 className="title">11º Concerto da OFC</h1>
+                    <p className="local"><strong>Local:</strong> Rua do CEFEC, Marcos Moura - Paraíba</p>
+                    <p className="programa"><strong>Programação:</strong> Músicas natalinas e Clássicos Nordestina</p>
+                </div>
+            </div>
         </div>
     )
 }
