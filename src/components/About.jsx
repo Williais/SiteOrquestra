fe357d3744f3da532e5416397112872ee2f59562
Willian Padilha <45665895+Williais@users.noreply.github.com>
2025-12-21 00:53:39 -0300

    Finalizando about component e responsividade

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,25 +6,25 @@ function About() {
         <div className='content'>
             <section className='maestro' >
                 <div className="img-sadraque">
-                    <img src={sadraque} alt="Foto do Maestro Sadraque" width={200}/>
+                    <img src={sadraque} alt="Foto do Maestro Sadraque" width={250}/>
                 </div>
-                <div className="frase">
+                <div className="frase-box">
                     <p id='frase'>"A música não é apenas arte, é cidadania."</p>
                     <p id='nome-frase'>- Maestro Sadraque Barreto</p>
                 </div>
             </section>
 
             <section className='sobre'>
-                <p id=''>QUEM SOMOS</p>
+                <p id='quem-somos'>QUEM SOMOS</p>
                 <h1>Nascidos do sonho <span>de transformar.</span></h1>
 
-                <p className='texto-Sobre'>A Orquestra CEFEC não é apenas um    corpo artístico; é a prova viva de que a cultura pode reescrever histórias. 
+                <p className='texto-Sobre'>A Orquestra Filarmônica do CEFEC não é apenas um corpo artístico, é a prova viva de que a cultura pode reescrever histórias. 
                     <br />
                     <br /> 
-                Integrada ao <a href="">Projeto CEFEC</a>, oferecemos formação de excelência para jovens músicos, criando pontes entre a música de concerto e a comunidade.
+                Integrada ao <a href="https://cefecpb.com.br" target='_blank'>CEFEC</a>, oferecemos formação de excelência para jovens músicos, criando pontes entre a música de concerto e a comunidade.
                 </p>
 
-                <a href=""> CONHEÇA NOSSA HISTÓRIA <span> → </span></a>
+                <a href="https://www.instagram.com/orquestracefec" target='_blank' id='conheca-link'> CONHEÇA NOSSA HISTÓRIA <span> → </span></a>
 
             </section>
         </div>
